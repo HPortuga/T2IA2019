881bfce660b0816c6a4595dfc9796257795e0cc9
Matheus Henrique <mathsoares1@gmail.com>
2019-06-03 18:21:42 -0400

    Comecado parse no arquivo iris

--- DataParser.py
+++ DataParser.py
@@ -1,2 +1,18 @@
-with open("./DataSets/iris-data.data", "w") as file:
-   file.write("HALLO")
+def parseIris():
+   dados = []
+   colunas = []
+   with open("./DataSets/Raw/iris.data", "r") as arquivo:
+      str = ""
+      for linha in arquivo:
+         for coluna in linha:
+            if (coluna != "," and coluna != "\n"):
+               str += coluna
+            else:
+               colunas.append(str)
+               str = "" 
+
+         dados.append(colunas)
+         colunas = []
+   dados.pop()
+
+parseIris()
