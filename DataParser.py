d6d7167aafe21e1a58706bd331dc5fd4410e81ae
Matheus Henrique <mathsoares1@gmail.com>
2019-06-06 08:14:42 -0400

    Parse do arquivo Iris

--- DataParser.py
+++ DataParser.py
@@ -13,6 +13,23 @@ def parseIris():
 
          dados.append(colunas)
          colunas = []
-   dados.pop()
+
+      data, target = divideOsDados(dados)
+
+def divideOsDados(dados):
+   indiceDaCategoria = 0
+   mapaCategorias = dict()
+   categorias = list()
+   for i in range(len(dados)):
+      dado = dados[i]
+      if not (dado[-1] in mapaCategorias):
+         mapaCategorias[dado[-1]] = indiceDaCategoria
+         indiceDaCategoria += 1
+
+      categorias.append(mapaCategorias[dado[4]])
+      del dado[-1]
+
+   return dados, categorias   
+      
 
 parseIris()
