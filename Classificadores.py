54cd0e8ab37f4c963a5b68a23210747c5c0f8a21
Matheus Henrique <mathsoares1@gmail.com>
2019-06-14 11:20:20 -0400

    alterando parametros possiveis

--- Classificadores.py
+++ Classificadores.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
          "splitter": ["best", "random"],
          "min_weight_fraction_leaf": np.arange(0, 0.5),
          "presort": [True, False],
-         "max_depth": np.arange(5, 10),
+         "max_depth": [10, 20, 30, 60, 100],
          "min_samples_split": np.arange(2, 5),
          "min_samples_leaf": np.arange(1, 5)
       }
@@ -73,38 +73,28 @@ if __name__ == "__main__":
          "n_neighbors": np.arange(5, 10),
          "weights": ["uniform", "distance"],
          "algorithm": ["auto", "ball_tree", "kd_tree", "brute"],
-         "leaf_size": np.arange(29, 34),
-         "p": np.arange(1, 6),
-         "metric" : ["minkowski"]
+         "leaf_size": np.arange(29, 32),
       }
 
       possiveisParamsNaiveBayes = {
-         "alpha": np.arange(0, 6),
+         "alpha": np.arange(0.0, 1.0, 0.2),
          "fit_prior": [True, False]
       }# MultinomialNB
 
       possiveisParamsRegressaoLogistica = {
-         "penalty": ["l1","l2","elasticnet","none"],
-         "dual": [True, False],
          "tol": np.arange(2.718**(-5), 2.718**(-3)),
          "fit_intercept": [True, False],
          "intercept_scaling": np.arange(1, 5),
-         "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"],
-         "max_iter": np.arange(90, 110),
-         "multi_class": ["ovr", "multinomial", "auto"]
+         "solver": ["lbfgs", "liblinear", "sag", "saga"],
+         "max_iter": [50, 100, 200, 250, 300],
       }
 
       possiveisParamsMLP = {
          "activation": ["identity", "logistic", "tanh", "relu"],
          "solver": ["lbfgs", "sgd", "adam"],
-         "alpha": np.arange(0.0001, 0.005),
          "learning_rate": ["constant", "invscaling", "adaptive"],
-         "learning_rate_init": np.arange(0.0001, 0.0005),
-         "power_t": np.arange(0.5, 1),
-         "max_iter": np.arange(200,205),
+         "max_iter": [50, 200, 300],
          "shuffle": [True, False],
-         "tol": np.arange(2.718**(-4), 2.718**(-2)),
-         "n_iter_no_change": np.arange(8, 13)
       }
 
       melhoresParams = list()
